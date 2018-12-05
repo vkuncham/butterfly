dd1afe4ae6e8d1f78720bc02d6a897529710c609
vkuncham <kvsharath_chandra@yahoo.com>
2018-12-05 17:49:33 -0600

    PomModel Changes

--- butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/operations/pom/PomModel.java
+++ butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/operations/pom/PomModel.java
@@ -1,11 +1,25 @@
 package com.paypal.butterfly.utilities.operations.pom;
 
-import com.paypal.butterfly.extensions.api.TOExecutionResult;
+import com.paypal.butterfly.extensions.api.*;
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 
-public class PomModel extends AbstractPomOperation<PomChangePackaging> {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+public class PomModel extends TransformationUtility {
 
     private String groupId, artifactId, version;
+    private static final String DESCRIPTION = "Retrieve the parent pom and load it in to Model Object";
+
+
+    public PomModel() {
+
+    }
 
     public PomModel(String groupId, String artifactId, String version) {
 
@@ -14,14 +28,35 @@ public class PomModel extends AbstractPomOperation<PomChangePackaging> {
         this.version=version;
     }
 
-    @Override
-    protected TOExecutionResult pomExecution(String relativePomFile, Model model) {
-        return null;
-    }
+
 
     @Override
     public String getDescription() {
-        return null;
+        return DESCRIPTION;
+    }
+
+    @Override
+    protected TUExecutionResult execution(File transformedAppFolder, TransformationContext transformationContext) {
+
+
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = null;
+
+        try (InputStream inputStream = new URL("https://repo1.maven.org/maven2/"+groupId+"/"+artifactId +"/" +version + "/" + artifactId+"-" +version +".pom").openStream();
+             FileOutputStream fileOS = new FileOutputStream("/Users/vkuncham/Documents/root.pom")){
+
+            int i = IOUtils.copy(inputStream, fileOS);
+
+            File parentPom = new File("/Users/vkuncham/Documents/root.pom");
+            FileInputStream fileInputStream = new FileInputStream(parentPom);
+            model = reader.read(fileInputStream);
+
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
+
+
+        return TUExecutionResult.value(this, model);
     }
 }
 
---
+++ butterfly-utilities/src/test/java/com/paypal/butterfly/utilities/operations/pom/PomModelTest.java
@@ -0,0 +1,24 @@
+package com.paypal.butterfly.utilities.operations.pom;
+
+import com.paypal.butterfly.extensions.api.TUExecutionResult;
+import com.paypal.butterfly.utilities.TransformationUtilityTestHelper;
+import com.paypal.butterfly.utilities.pom.PomGetPackaging;
+import org.apache.maven.model.Model;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.mockito.Mockito;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
+
+public class PomModelTest extends TransformationUtilityTestHelper {
+
+    @Test
+    public void descriptionTest() throws Exception {
+
+
+    }
+
+
+}
